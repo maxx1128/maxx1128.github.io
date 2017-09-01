8eaf84570ad33f78412ab215ad0e88599e00b1d1
Max Antonucci <maxx1128@gmail.com>
2017-09-01 09:30:01 -0400

    Added feather dependency

--- _javascript/main.js
+++ _javascript/main.js
@@ -4,6 +4,9 @@ let f = require('./functions.js');
 let a11y_toggle = require('a11y-toggle/a11y-toggle.js');
 let color_schemes = require('./css-themes.js');
 let wisdom = require('./wisdom.js');
+let feather = require('feather-icons/dist/feather.min.js');
+
+feather.replace();
 
 wisdom.activate();
 
